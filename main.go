987d5eaf79d4b2978d6238fc7725aae4cec8d3af
Ivan Krasin <krasin@robodone.com>
2017-05-13 15:47:37 -0700

    Open and close serial port. Don't sent any commands yet.

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
+
+	"github.com/samofly/serial"
 )
 
 var (
-	dev      = flag.String("dev", "", "Device to connect to the printer, such as /dev/ttyUSB0 or /dev/ttyACM0")
+	ttyDev   = flag.String("dev", "", "Device to connect to the printer, such as /dev/ttyUSB0 or /dev/ttyACM0")
 	baudRate = flag.Int("rate", 115200, "Baud rate")
 	gcode    = flag.String("gcode", "", "gcode file to print")
 )
@@ -21,13 +24,23 @@ func failf(format string, args ...interface{}) {
 	os.Exit(1)
 }
 
+func logf(format string, args ...interface{}) {
+	log.Printf(format, args...)
+}
+
 func main() {
 	flag.Parse()
 
-	if *dev == "" {
+	if *ttyDev == "" {
 		failf("--dev not specified")
 	}
 	if *gcode == "" {
 		failf("--gcode not specified")
 	}
+	conn, err := serial.Open(*ttyDev, *baudRate)
+	if err != nil {
+		failf("Could not open serial port %s at %d bps. Error: %v", *ttyDev, *baudRate, err)
+	}
+	defer conn.Close()
+	logf("Opened %s at %d bps.", *ttyDev, *baudRate)
 }
