eb3a2d342877189d2426b27dcad5582c8867a302
Ivan Krasin <krasin@robodone.com>
2017-06-02 09:30:52 -0700

    Adjust the code to the repository rename agent -> robosla-agent.

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -4,7 +4,7 @@ readonly SUFFIX=${1}
 
 set -ue
 
-cd ~/src/github.com/robodone/agent
+cd ~/src/github.com/robodone/robosla-agent
 
 
 if [[ -z  "$SUFFIX"  ]] ; then
@@ -16,14 +16,14 @@ readonly RELEASE="$(date +%Y-%m-%d)-$1"
 echo "Making a release ${RELEASE}"
 
 echo "Building from source ..."
-GOARCH=amd64 go get github.com/robodone/agent
-GOARCH=arm GOARM=7 go get github.com/robodone/agent
+GOARCH=amd64 go get github.com/robodone/robosla-agent
+GOARCH=arm GOARM=7 go get github.com/robodone/robosla-agent
 
 echo "Patching robosla-agent.json manifest ..."
 cp -f robosla-agent.json /tmp/robosla-agent.json
 sed -i "s/RELEASE_NAME/${RELEASE}/" /tmp/robosla-agent.json
 
 echo "Copying to Google Cloud Storage ..."
-gsutil cp -a public-read ~/bin/agent "gs://robosla-agent/${RELEASE}/amd64/agent"
-gsutil cp -a public-read ~/bin/linux_arm/agent "gs://robosla-agent/${RELEASE}/arm/agent"
+gsutil cp -a public-read ~/bin/robosla-agent "gs://robosla-agent/${RELEASE}/amd64/robosla-agent"
+gsutil cp -a public-read ~/bin/linux_arm/robosla-agent "gs://robosla-agent/${RELEASE}/arm/robosla-agent"
 gsutil cp -a public-read /tmp/robosla-agent.json gs://robosla-agent/robosla-agent.json
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/robodone/agent/gcode"
+	"github.com/robodone/robosla-agent/gcode"
 	"github.com/samofly/serial"
 )
 
