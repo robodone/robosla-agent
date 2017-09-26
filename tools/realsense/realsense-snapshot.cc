a5602e20f51250d6b69ac6cebd89a378c020dcc6
Ivan Krasin <krasin@robodone.com>
2017-09-25 22:23:10 -0700

    realsense-snapshot: initialize streams and wait for the first 60 frames.

--- tools/realsense/realsense-snapshot.cc
+++ tools/realsense/realsense-snapshot.cc
@@ -1,8 +1,54 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include <librealsense/rs.hpp>
 
+const int kSkipFirstFrames = 60;
+
+void fail(const char* msg) {
+  fprintf(stderr, "%s\n", msg);
+  _exit(1);
+}
+
 int main(void) {
-  printf("Hello world!\n");
+  rs::log_to_console(rs::log_severity::warn);
+
+  // Create a context.
+  rs::context ctx;
+
+  // Find and open a device.
+  if (!ctx.get_device_count()) {
+    fail("No RealSense devices connected");
+  }
+
+  rs::device * dev = ctx.get_device(0);
+  fprintf(stderr, "RealSense device opened: %s, SN %s, firmware version %s\n",
+	  dev->get_name(), dev->get_serial(), dev->get_firmware_version());
+
+  // Check that the camera supports color and depth streams and enable them.
+  if (!dev->supports(rs::capabilities::color)) {
+    fail("Device does not support RGB stream");
+  }
+  if (!dev->supports(rs::capabilities::depth)) {
+    fail("Device does not support Depth stream");
+  }
+
+  dev->enable_stream(rs::stream::color, rs::preset::best_quality);
+  dev->enable_stream(rs::stream::depth, rs::preset::best_quality);
+
+  // Start streaming.
+  dev->start();
+
+  // Get stream params (including width and height of each frame).
+  rs::intrinsics color_intrinsics = dev->get_stream_intrinsics(rs::stream::color);
+  rs::intrinsics depth_intrinsics = dev->get_stream_intrinsics(rs::stream::depth);
+
+  // Skip first few frames to make sure we have a stable image.
+  for (int i = 0; i < kSkipFirstFrames; i++) {
+    dev->wait_for_frames();
+  }
+
+  // TODO(krasin): get actual data from the streams.
+
   return 0;
 }
