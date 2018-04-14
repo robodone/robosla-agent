7465331a1824afec135e879725f724e2e0ba60f2
Ivan Krasin <krasin@robodone.com>
2018-04-14 15:47:04 -0700

    raspistill: remove 5s delay. Now images are taken within ~0.8s.
    A better approach would be to use the signalling mode (-s).

--- raspistill.go
+++ raspistill.go
@@ -20,7 +20,7 @@ func (rss *RaspistillSnapshotter) TakeSnapshot(ctx context.Context, prefix strin
 	defer rss.mu.Unlock()
 
 	fname := fmt.Sprintf("%s%02d-camera0.jpg", prefix, 0)
-	cmd := exec.Command("/usr/bin/raspistill", "-w", "640", "-h", "480", "-q", "65", "-o", fname)
+	cmd := exec.Command("/usr/bin/raspistill", "-w", "640", "-h", "480", "--timeout", "1", "-q", "65", "-o", fname)
 	data, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("failed to take a raspistill snapshot: %v\nCombined output:\n%v", err, string(data))
