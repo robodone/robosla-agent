c683fd7a6c03dc59520211e9cba4ccd84e902020
Ivan Krasin <imkrasin@gmail.com>
2018-04-29 17:31:41 -0700

    Implement CombinedSnapshotter, but don't use it yet.

--- snapshot.go
+++ snapshot.go
@@ -1,7 +1,40 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"sync"
+)
 
 type Snapshotter interface {
 	TakeSnapshot(ctx context.Context, prefix string, numFrames int) error
+}
+
+type CombinedSnapshotter struct {
+	Snaps map[string]Snapshotter
+}
+
+func (cs *CombinedSnapshotter) TakeSnapshot(ctx context.Context, prefix string, numFrames int) error {
+	if numFrames != 1 {
+		return fmt.Errorf("only taking a single snapshot is supported by CombinedSnapshot, but %d was requested", numFrames)
+	}
+	errs := make(map[string]error)
+	var wg sync.WaitGroup
+	wg.Add(len(cs.Snaps))
+	for name, snap := range cs.Snaps {
+		go func(name string, snap Snapshotter) {
+			defer wg.Done()
+			err := snap.TakeSnapshot(ctx, prefix, numFrames)
+			if err != nil {
+				errs[name] = err
+			}
+		}(name, snap)
+	}
+	wg.Wait()
+	for name, err := range errs {
+		if err != nil {
+			return fmt.Errorf("failed to take a %s snapshot: %v", name, err)
+		}
+	}
+	return nil
 }
